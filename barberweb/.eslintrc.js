b48b89f8bd98e2024ebfe3db4a0038554509b3de
Felipe Bueno <felipebueno181@gmail.com>
2019-11-27 21:13:26 -0300

    Layout por página | templates

--- barberweb/.eslintrc.js
+++ barberweb/.eslintrc.js
@@ -33,6 +33,6 @@ module.exports = {
       "react-hooks/rules-of-hooks": "error",
       "react-hooks/exhaustive-deps": "warn",
       "react/jsx-props-no-spreading": "off",
-      "react/no-typos": "off"
+      "react/prop-types": "off"
     }
   };
