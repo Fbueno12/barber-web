ec843dc091f7537e10a22de746b097d56f2d407d
Felipe Bueno <felipebueno181@gmail.com>
2019-11-27 21:21:46 -0300

    Estilizacao global

--- barberweb/src/App.js
+++ barberweb/src/App.js
@@ -6,10 +6,13 @@ import './Config/ReactotronConfig';
 import Routes from './routes';
 import history from './services/history';
 
+import GlobalStyle from './styles/global';
+
 function App() {
     return (
         <Router history={history}>
             <Routes />
+            <GlobalStyle />
         </Router>
     );
 }
